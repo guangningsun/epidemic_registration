2dc1a5730c59fa276ba41edbec293c0d74c0365d
sunguangning <sunguangning@126.com>
2020-11-28 23:04:09 +0800

    fix family id se

--- server/AppModel/serializer.py
+++ server/AppModel/serializer.py
@@ -14,4 +14,4 @@ class CheckinSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = CheckInfo
-        fields = ('family_contact_name','family_tel_num','family_address','registerTime','checkin_status','family_member_num','family_member_id','name','gender','age','nation','id_num','tel_num','address','work_place','has_disease_radio','disease_name','medicine_name','has_take_medicine_radio','room','hotel')
+        fields = ('family_contact_name','family_tel_num','family_address','registerTime','checkin_status','family_member_num','family_id','name','gender','age','nation','id_num','tel_num','address','work_place','has_disease_radio','disease_name','medicine_name','has_take_medicine_radio','room','hotel')
